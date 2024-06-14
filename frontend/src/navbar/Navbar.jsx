2d8e0eeba6cc766f58825883a00214e598d31ec2
Efren707 <53492872+Efren707@users.noreply.github.com>
2024-06-14 18:47:23 -0500

    add navbar styling

--- frontend/src/navbar/Navbar.jsx
+++ frontend/src/navbar/Navbar.jsx
@@ -1,53 +1,69 @@
 import "./styles/navbar.css";
 import React from 'react';
+import { useSelector } from "react-redux";
 
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Emaiacon from '@mui/icons-material/Email';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function Navbar() {
-  return (
-    <div className='navContainer'>
 
-        <div className="navSubContainer">
+    const {_id, userName, email, password, profilePicPath, followers, following} = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
 
-            <div className="navLeft">
-                <a className="navLogoContainer">
-                    <div className="navLogo"></div>
-                </a>
-                <a className="navLink">Home</a>
-                <a className="navLink">Feed</a>
-                <a className="navLink">Library</a>
-            </div>
-            
-            <a className="navSearchBar">
-                <input type="text"/>
-            </a>
-            
-            <div className="navRight">
-                <a className="navUpload">Upload</a>
-            
-                <a className="navProfile">
-                    <img/>
-                </a>
+    return (
+        <div className='navContainer'>
+
+            <div className="navSubContainer">
+
+                <div className="navLeft">
+                    <a className="navLogoContainer">
+                        <div className="navLogo"></div>
+                    </a>
+
+                    <div className="navLeftLinks">
+                        <a className="navLink">Home</a>
+                        <a className="navLink">Feed</a>
+                        <a className="navLink">Library</a>
+                    </div>
+                </div>
                 
-                <a className="navIcons">
-                    <NotificationsIcon/>
-                </a>
+                <div className="navSearchBar">
+                    <input type="text"/>
+                </div>
+                
+                <div className="navRight">
 
-                <a className="navIcons">
-                    <Emaiacon/>
-                </a>
+                    <div className="navRightSubContainer">
+                        <a className="navUpload">Upload</a>
+                
+                        <a className="navProfile">
+                            <img src={`https://efren-soundcloud-storage.s3.us-east-2.amazonaws.com/profilePicture/${profilePicPath}`}/>
+                            <ExpandMoreIcon/>
+                        </a>
 
-                <a className="navIcons">
-                    <MoreHorizIcon/>
-                </a>
+                        <div className="navRightIcons">
+                            <a className="navIcons">
+                                <NotificationsIcon/>
+                            </a>
+
+                            <a className="navIcons">
+                                <Emaiacon/>
+                            </a>
+
+                            <a className="navIcons">
+                                <MoreHorizIcon/>
+                            </a>
+                        </div>
+                    </div>
+                    
+                </div>
+
             </div>
-
+            
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default Navbar;
